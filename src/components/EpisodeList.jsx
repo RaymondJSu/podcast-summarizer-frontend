86843b325893a758821a6739239c9017bada9a13
Raymond Su <raymondsu@Raymonds-MacBook-Pro.local>
2024-09-16 16:52:29 -0700

    fix loading issue

--- src/components/EpisodeList.jsx
+++ src/components/EpisodeList.jsx
@@ -10,6 +10,9 @@ const EpisodeList = () => {
   useEffect(() => {
     const fetchEpisodes = async () => {
       try {
+        console.log("fetching episodes")
+        console.log(apiUrl)
+        console.log(process.env.REACT_APP_API_URL)
         const response = await fetch(apiUrl);
         const data = await response.json();
         setEpisodes(data);
